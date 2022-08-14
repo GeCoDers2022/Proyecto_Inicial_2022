58f0e7f8eb1c626856ea5c5a677998b0776dbeb8
Gabriel Juhasz <gabrieljaj@gmail.com>
2022-08-14 01:53:07 -0300

    Agregado Ejercicio 9 Base de Datos  G.Juhasz

--- ISPC-BD-GJuhasz.sql
+++ ISPC-BD-GJuhasz.sql
@@ -12,14 +12,24 @@ Perro int not null, Descripcion varchar(300) not null, Monto int,
 CONSTRAINT FOREIGN KEY (Perro) PREFERENCES Perro(ID_Perro)
 )
 
+/* Carga de datos en las tablas para prueba*/
+INSERT INTO Dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (84245823, "Ignacio", "DiPaolo", 1178529845, "calle Publica s/n");
+INSERT INTO Dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (76249823, "Jacobo", "Benetto", 1137595566, "calle Publica s/n");
+INSERT INTO Dueno (DNI, Nombre, Apellido, Telefono, Direccion) VALUES (65244354, "Nahuel", "Castelli", 1153784355, "calle Publica s/n");
+
+INSERT INTO Perro (Nombre, Fecha_nac, Sexo, DNI_dueno) VALUES ("Tintin","2020-04-23", "Macho", 84245823);
+INSERT INTO Perro (Nombre, Fecha_nac, Sexo, DNI_dueno) VALUES ("Firu","2017-11-16", "Hembra", 76249823);
+INSERT INTO Perro (Nombre, Fecha_nac, Sexo, DNI_dueno) VALUES ("Roco","2014-08-28", "Macho", 65244354);
+
+INSERT INTO Historial (Fecha, Perro, Descripcion, Monto) VALUES ("2022-12-05", "2", "Pulgicida", 430);
+INSERT INTO Historial (Fecha, Perro, Descripcion, Monto) VALUES ("2022-03-19", "3", "Alimento Balanceado", 1200);
+INSERT INTO Historial (Fecha, Perro, Descripcion, Monto) VALUES ("2022-07-13", "1", "Sobre Saborizado", 250);
 
 
-/*Ejercicio 3*/
+/* Ejercicio9. Escriba una consulta que permita actualizar la dirección de un dueño. Su nueva dirección es Libertad 123 */
 
-DELETE FROM Perro inner join Historial on Perro.ID_Perro = Historial.Perro
-
-Where (Historial.Fecha) = min (Historial.Fecha);
-
-
+UPDATE Dueno 
+SET Direccion = "Libertad 123" 
+WHERE DNI = 84245823;
 
 
